7b5d7de00b47bd164c9921e55c2f412b7e1553c2
cirruselli <cirruselli@gmail.com>
2021-05-26 05:28:41 +0000

    Remove secret_key and set debug

--- ellis/settings.py
+++ ellis/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4e=(c70tof-92#r571f+_gtor3xpn8_1dam=#tfs1tyj5r6@z0'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['cirruselli-ellis.herokuapp.com', 'localhost']
 
